36930e2267cfa9a94865ce4d22b8fe11ca3f70a1
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-12 16:40:55 +0200

    test

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'version': '0.1',
     'license': 'AGPL-3',
     # any module necessary for this one to work correctly
-    'depends': ['base','arope-conf'],
+    'depends': ['base',],
 
     # always loaded
     'data': [
