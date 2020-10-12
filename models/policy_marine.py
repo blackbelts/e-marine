3b0e96c574389926c919eec2bd276fa2ea06f7f0
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-10-12 16:36:08 +0200

    test

--- models/policy_marine.py
+++ models/policy_marine.py
@@ -94,7 +94,7 @@ class PolicyMarine(models.Model):
       agency_branch=fields.Many2one('agency.branch.marine',string='Shipping Branch')
       currency_id=fields.Many2one('res.currency',string='Currency',required=True)
       endorsement_no = fields.Integer(string="Endorsement Number")
-      broker= fields.Many2one('persons',string="Broker" ,domain="[('type', '=', 'broker')]")
+      broker= fields.Many2one('res.users',string="Broker" )
       broker_pin = fields.Char(string="Agent Code")
       broker_fra_code = fields.Char(string="Broker FRA Code")
       broker_commission = fields.Float(string="Broker Commission")
